ef51cbc3183e20d7928332daff5190efc9dc4511
qfgao <canchiqingting@yahoo.com>
2016-08-17 18:54:16 +0800

    0.0.7

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
-	return render(request, "html/index.php")
+	return render(request, "html/wp-blog-header.php")
