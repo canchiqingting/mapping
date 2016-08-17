8e825543c8231a2a1e95e7e368283f57a42f280d
qfgao <canchiqingting@yahoo.com>
2016-08-17 17:48:45 +0800

    0.0.5

--- map/views.py
+++ map/views.py
@@ -5,8 +5,6 @@ import json
 def index(request):
 	dict = {}
 	fruit = Person(username = "qfgao",password = "qw1990")
-#	fruit.username = "qfgao"
-#	fruit.password = "qw1990"
 	fruit.save()
 	dict["username"] = "qfgao"
 	dict["password"] = Person.objects.get(pk=1).password
