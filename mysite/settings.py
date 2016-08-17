66c3f6bdc4e18fdd137e24b10bdbb89d5009fdc2
qfgao <canchiqingting@yahoo.com>
2016-08-17 18:10:47 +0800

    0.0.6

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                 '/var/www/html/mysite',
+         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
