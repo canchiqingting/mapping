b8147cf97011516ca470da05cda723a4a57eb7e1
qfgao <canchiqingting@yahoo.com>
2016-08-17 16:45:16 +0800

    0.0.4

--- map/views.py
+++ map/views.py
@@ -4,16 +4,12 @@ from models import Person
 import json
 def index(request):
 	dict = {}
-#	info = 'Json success'
-#	if request.method == 'POST':
-#		req = simplejson.loads(request.raw_post_data)
-#		username = request.GET.getet('username') 
-	#	info = request.raw_post_data
 	fruit = Person(username = "qfgao",password = "qw1990")
 #	fruit.username = "qfgao"
 #	fruit.password = "qw1990"
 	fruit.save()
 	dict["username"] = "qfgao"
-	dict["password"] = fruit.get_username()
+	p = Person.objects.filter(username = "qfgao")
+	dict["password"] = p.password
 	m_json = json.dumps(dict)
 	return HttpResponse(m_json)
